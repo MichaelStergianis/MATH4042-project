b2d31e33c8d9c08dc31f4c052ff119036630bb98
Michael Stergianis <michaelstergianis@gmail.com>
2018-03-19 10:51:42 -0400

    Fix up gaussian. Add stub for median.

--- program/src/filters.py
+++ program/src/filters.py
@@ -5,10 +5,21 @@ def gaussian1d(x, mu, sigma):
     return g
 
 def gaussian(size, mu=np.array([0, 0]), sigma=np.array([1.0, 1.0])):
-    """Produces a gaussian filter conforming to the parameters provided"""
+    """Produces a gaussian filter function conforming to the parameters provided"""
     # initialize our kernel to be a square kernel
     x = np.arange(size).reshape([-1, 1]) - (size // 2)
 
     g1 = gaussian1d(x, mu[0], sigma[0])
     g2 = gaussian1d(x.T, mu[1], sigma[1])
-    return np.dot(g1, g2)
+    k = np.dot(g1, g2)
+
+    # create a function that can be used like our filtering interface
+    def kernel(patch):
+        return patch * k
+
+    return kernel
+
+def median(patch):
+    """Returns the median value of a patch"""
+    # TODO Leanna move from ipython to python
+    pass
