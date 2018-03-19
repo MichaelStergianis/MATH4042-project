069b7fd698ec59749630611c150514e342e838a6
Michael Stergianis <michaelstergianis@gmail.com>
2018-03-19 14:31:40 -0400

    Add function stubs

---
+++ program/src/statistics.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+def rmse(img, ground_truth):
+    """Calculates the root mean squared error of an image"""
+    pass
+
+
+def snr(img, ground_truth):
+    """Calculates the signal to noise ratio of an image"""
+    pass
