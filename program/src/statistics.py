6bc81d4a18a8e2630c81bc6bcb259086b47112d1
Michael Stergianis <michaelstergianis@gmail.com>
2018-03-26 16:02:58 -0400

    Add a program to compute stats.

--- program/src/statistics.py
+++ program/src/statistics.py
@@ -5,18 +5,43 @@ import cv2
 def rmse(img, ground_truth):
     """Calculates the root mean squared error of an image"""
     m, n, k = ground_truth.shape
-    rmse_val = 1/(m*n*k)*np.linalg.norm(ground_truth - img)
+    rmse_val = (1 / (m * n * k)) * np.linalg.norm(ground_truth - img)
 
     return rmse_val
 
 
 def snr(img, ground_truth):
     """Calculates the signal to noise ratio of an image"""
-    snr_val = 20*np.log10(np.linalg.norm(ground_truth)/np.linalg.norm(img))
+    snr_val = 20 * np.log10(np.linalg.norm(ground_truth) / np.linalg.norm(img))
     return snr_val
 
 
-# Leanna's test data
-#pep = cv2.imread('/home/leanna/Documents/4 year Winter/Imaging/noisy_peppers.png')
-#gt = cv2.imread('/home/leanna/Documents/4 year Winter/Imaging/truth_peppers.png')
-#sprint(rmse(pep, gt))
+def main():
+    import argparse
+    ap = argparse.ArgumentParser()
+    ap.add_argument(
+        'img', type=str, help='The image on which to compute statistics')
+    ap.add_argument(
+        'ground',
+        type=str,
+        help='The ground truth image on which to compute statistics')
+    ap.add_argument(
+        '--snr',
+        action='store_true',
+        help='Whether or not to compute the snr value')
+    ap.add_argument(
+        '--rmse',
+        action='store_true',
+        help='Whether or not to compute the rmse value')
+
+    args = ap.parse_args()
+
+    if args.snr:
+        print(snr(args.img, args.ground))
+
+    if args.rmse:
+        print(rmse(args.img, args.ground))
+
+
+if __name__ == '__main__':
+    main()
