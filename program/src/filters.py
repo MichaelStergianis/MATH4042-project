5de24bb34a299e30c16e536b35e1aea80c4a4270
Michael Stergianis <michaelstergianis@gmail.com>
2018-03-25 20:48:46 -0400

    Stub for improved median.

--- program/src/filters.py
+++ program/src/filters.py
@@ -22,3 +22,34 @@ def gaussian(size, mu=np.array([0, 0]), sigma=np.array([1.0, 1.0])):
 def median(patch):
     """Returns the median value of a patch"""
     return np.median(patch)
+
+
+def normalized_mean(patch):
+    """Computes a new mean, to be used later in denoising"""
+    n, m, _ = patch.shape
+    patch_vec = patch.reshape([n * m, 1])
+    middle_element = (len(patch_vec) - 1) / 2
+    median = np.median(patch_vec)
+    patch_vec[middle_element] = median
+    new_median = np.median(patch_vec)
+    return np.mean(patch_vec)
+
+
+def improved_median(patch):
+    """A median filter that makes decisions about how to compute the return value"""
+    ## central pixel is swapped with median
+    half_width = patch.size[0] // 2
+    # copy the array
+    med_patch = np.array(patch)
+    med_patch[half_width, half_width] = np.median(patch)
+
+    ## new average is computed
+    avg = med_patch.mean()
+
+    ## compare each value with new average
+
+
+    ## if all values are greater than average take median of patch and return
+
+    ## if even one of the values is less than the average leave that pixel alone
+    return
